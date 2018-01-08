1e7ac6fc322dfedbac02fba1455fb111b6e8adda
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-01-08 19:25:43 +0100

    ehbé

--- installes-tout.sh
+++ installes-tout.sh
@@ -1,9 +1,9 @@
 # installation openssh
 # sudo apt-get remove -y openssh-server
 # sudo apt-get install -y openssh-server
-clear
-echo "ne cherches pas à comprendre, appuies 4 fois (comptes bien 4) sur Entrée pour continuer"
-read
+# clear
+# echo "ne cherches pas à comprendre, appuies 4 fois (comptes bien 4) sur Entrée pour continuer"
+# read
 
 # ssh-keygen -t rsa -b 4096
 # installation docker
