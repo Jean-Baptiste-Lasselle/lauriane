e24195e28c4ce41620b7c7ef0433080a3d05adb0
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-01-11 10:12:49 +0100

    commencé ajout conteneur mariadb, TODO: remote cleint access , cf https://mariadb.com/kb/en/library/configuring-mariadb-for-remote-client-access/

--- installes-tout.sh
+++ installes-tout.sh
@@ -79,13 +79,52 @@ usermod -aG docker $USER
 systemctl enable docker
 systemctl start docker
 
+
+clear
 ##########################################################
 ##### MONTEE INFRASTRUCTURE CIBLE DE DEPLOIEMENT #########
 ##########################################################
-# construction d'un conteneur tomcat 
+
+# >>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<
+# > construction du conteneur SERVEUR JEE  <
+# >>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<
 #
-NOM_CONTENEUR_TOMCAT=ciblededeploiement-composant-srv-jee
-# NUMERO_PORT_SRV_JEE=8888
-clear
-docker run -it --name $NOM_CONTENEUR_TOMCAT --rm -p $NUMERO_PORT_SRV_JEE:8080 tomcat:8.0
+# NOM_CONTENEUR_TOMCAT=ciblededeploiement-composant-srv-jee
+
+VERSION_TOMCAT=10.1
+
+# docker run -it --name $NOM_CONTENEUR_TOMCAT --rm -p $NUMERO_PORT_SRV_JEE:8080 tomcat:8.0
+docker run --name $NOM_CONTENEUR_TOMCAT --rm -p $NUMERO_PORT_SRV_JEE:8080 tomcat:$VERSION_TOMCAT
 # http://adressIP:8888/
+
+
+# >>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<
+# >     construction du conteneur SGBDR    <
+# >>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<
+#
+# NOM_CONTENEUR_SGBDR=ciblededeploiement-composant-sgbdr
+# NUMERO_PORT_SGBDR=3308
+DB_NAME=bdd_appli_lauriane
+
+# Avec cet utilisateur, on va créer la BDD $DB_NAME, et
+# créer l'utilisateur $DB_APP_USER_NAME
+#
+DB_MGMT_USER_NAME=lauriane
+DB_MGMT_USER_PWD=lauriane
+
+DB_APP_USER_NAME=appli-de-lauriane
+DB_APP_USER_PWD=mdp@ppli-l@urian3
+
+VERSION_MARIADB=10.1
+
+# > créer le conteneur avec usr, et root_user
+# La "--collation-server" permet de définir l'ordre lexicographique des mots formés à partir de l'alphabet définit par le jeu de caractères utilisé
+# La "--character-set-server" permet de définir l'encodage et le jeu de caractères utilisé
+docker run --name $NOM_CONTENEUR_SGBDR -e MYSQL_ROOT_PASSWORD=peuimporte -e MYSQL_USER=$DB_MGMT_USER_NAME -e MYSQL_PASSWORD=$DB_MGMT_USER_PWD -p $NUMERO_PORT_SGBDR:3306 -d mariadb:$VERSION_MARIADB  --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+# > configurer l'accès "remote" pour les 2 utilisateurs  $DB_MGMT_USER_NAME  et  $DB_APP_USER_NAME
+# https://mariadb.com/kb/en/library/configuring-mariadb-for-remote-client-access/
+
+# > créer la BDD
+docker exec -it $NOM_CONTENEUR_SGBDR /bin/bash
+# > mysql_secure_installation
