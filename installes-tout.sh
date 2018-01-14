819fd659af1b85e1a18ff9cc6a6b09aa6f4fa8dc
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-01-14 21:28:37 +0100

    correction erreur pour l'exéction des scripts des opérations de création / configuration des utilisateurs SQL dans mariaDB. /bin/bash -c :AJOUT D'UN POINT DE DEBUG

--- installes-tout.sh
+++ installes-tout.sh
@@ -242,17 +242,27 @@ echo " ==>>> Maintenant, on va créer BDD et Utilisateur Applicatif."
 read
 #
 # exécution de la création bdd et utilisateur applicatif 
+clear
 sudo docker exec $NOM_CONTENEUR_MARIADB /bin/bash -c "/creer-bdd-application.sh"
 sudo docker exec $NOM_CONTENEUR_MARIADB /bin/bash -c "/creer-utilisateur-applicatif.sh"
 sudo docker exec $NOM_CONTENEUR_MARIADB /bin/bash -c "/configurer-utilisateur-mgmt.sh"
 # docker restart $NOM_CONTENEUR_TOMCAT
 # > configurer l'accès "remote" pour les 2 utilisateurs  $DB_MGMT_USER_NAME  et  $DB_APP_USER_NAME
 # https://mariadb.com/kb/en/library/configuring-mariadb-for-remote-client-access/
-clear
+# clear
+echo " "
+echo " "
+echo " "
 echo POINT DEBUG 2
 echo VERIF CONF DS CONTENEUR SGBDR utilisateurs : 
+echo "   les actions ont été réalisées "
+echo "   ----------------- "
+echo "    "
+echo "   ----------------- "
+
 echo "   utilisateur mgmt "
 echo "   ----------------- "
+
 echo "   utilisateur=$MARIADB_DB_MGMT_USER_NAME"
 echo "   mot de passe=$MARIADB_DB_MGMT_USER_PWD"
 echo "   "
