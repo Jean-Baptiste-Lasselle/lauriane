ed377ad20004d94681ce96a7f65f0e7ba4683359
Jean Baptiste Lasselle <jean.baptiste.lasselle.it@gmail.com>
2018-03-13 15:49:17 +0100

    mise à jour des commentaires

--- monter-cible-deploiement-scala.sh
+++ monter-cible-deploiement-scala.sh
@@ -32,18 +32,15 @@ fi
 export PROVISIONNING_HOME=$HOME/provisionning-scala
 # rm -rf $PROVISIONNING_HOME
 # mkdir -p $PROVISIONNING_HOME
-# à demander interactivement à l'utilisateur: "DAns quel répertoire souhaitez-vous que l'application scala soit déployée? C'est dans ce répertoire que la commande st sera lancée. [Par défaut, le répertoire utilsié sera le répertoire ..]:"
+# à demander interactivement à l'utilisateur: "Dans quel répertoire souhaitez-vous que l'application scala soit déployée? C'est dans ce répertoire que la commande st sera lancée. [Par défaut, le répertoire utilsié sera le répertoire ..]:"
 export REPERTOIRE_APP_SCALA_DS_CIBLE_DEPLOIEMENT=$HOME/software-factory-website
 # export WHERE_I_WAS_CLONED=$PROVISIONNING_HOME/recettes-operations
 # à demander interactivement à l'utilisateur: "Quel utilisateur linux souhaitez-vous que le deployeur-maven-plugin utilise?"
 export MVN_PLUGIN_OPERATEUR_LINUX_USER_NAME=lauriane
-# lors de son installation, PostGReSQL créée un utilisateur linux, avec
+# Lors de son installation, PostGReSQL créée un utilisateur linux, avec
 # lequel il est possible d'utiliser le client psql (pour crééer/modifier/détruire des BDD)
-# Ce nom d'utilisateur linux est utilisé pour la provision scala, et pour sa configuration sudoers cf. [recette-provisionning-lx-user-provision-scala.sh]
+# Ce nom d'utilisateur linux est utilisé pour la provision de la BDD utilisée par l'applciation Scala déployée.
 export USER_SQL_CREE_PAR_INSTALL_POSTGRE=postgres
-
-# Pas tant qu'on a une dépendance au script de lauraine, lulu... ======================>>>> dépendance
-# export REPERTOIRE_APP_SCALA_DS_CIBLE_DEPLOIEMENT/home/lauriane/tulavuvlulu
 
 # création du répertoire de travail pour le provisionning
 rm -rf $REPERTOIRE_APP_SCALA_DS_CIBLE_DEPLOIEMENT
